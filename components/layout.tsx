016644f10c679e9af00c8f7f17f7e8bf6b0ace1a
Timur Khazamov <bboytx@gmail.com>
2021-11-12 23:50:55 +0100

    Fixup git file-name case issue

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from './Footer'
-import Header from './Header'
+import Footer from './footer'
+import Header from './header'
 
 const Layout: React.FC<{}> = ({ children }) => {
   return (
